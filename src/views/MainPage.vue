b26ca001d7821981cf7a86a2459ae9bd0705a23e
RamilAsadullin <r.asadullin@danaflex.ru>
2024-01-26 03:17:57 +0300

    clear project

---
+++ src/views/MainPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+
+export default {
+  name: 'MainPage',
+  components: {
+  },
+  props: {
+  },
+  setup () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
